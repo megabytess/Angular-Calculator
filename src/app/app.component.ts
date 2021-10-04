9d59a5455b5d00767562311bbd45bc4dd39f1395
Christian <christianrey.capoy.20@usjr.edu.ph>
2021-10-04 16:48:10 +0800

    added angular-calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Calculator';
+  number1!:number;
+  number2!:number;
+  result!:number;
+
+  getValue(value:any){
+    this.number1 = parseInt(value);
+  }
+
+  getValue2(value:any){
+    this.number2 = parseInt(value);
+  }
+
+  add(){
+    this.result=this.number1+this.number2
+  }
+
+  sub(){
+    this.result=this.number1+this.number2
+  }
+
+  multi(){
+    this.result=this.number1+this.number2
+  }
+
+  div(){
+    this.result=this.number1+this.number2
+  }
 }
+
